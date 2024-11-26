d49786932bc411477cc4c513e656e1c7aa4714d3
michaudrd <Renee.Michaud@edu.sait.ca>
2024-11-26 02:32:45 -0700

    11-26-2024 Update: Small pet card modification for HomePage testing, can be removed as needed

--- src/components/pet-card.js
+++ src/components/pet-card.js
@@ -13,7 +13,7 @@ export default function PetCard(props) {
   const petUrl = url || `/pet/${pet.id}`;
 
   return (
-    <Link href={url}>
+    <Link href={petUrl}>
       <div className="flex flex-col shadow-md bg-brandWhite p-8 rounded-md">
         <div className="flex justify-center w-[420px] h-[420px]">
           <img
